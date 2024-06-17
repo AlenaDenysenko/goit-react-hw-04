23c3288c78981587d7b259b37f945aedfb102e85
AlenaDenysenko <naopiya28@gmai.com>
2024-06-17 15:40:15 +0300

    HW-4

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+// src/App.jsx
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
+import styles from './App.module.css';
+
+const API_KEY = 'v8Yz4MWJbqwoMFcY7cvCAKgXN3eAWzUE_29ha-bVlA0';
+const BASE_URL = 'https://api.unsplash.com/search/photos';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchImages = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await axios.get(BASE_URL, {
+          params: {
+            query,
+            page,
+            per_page: 12,
+            client_id: API_KEY,
+          },
+        });
+
+        setImages((prevImages) => [...prevImages, ...response.data.results]);
+      } catch (error) {
+        setError('Failed to fetch images');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, [query, page]);
+
+  const handleSearchSubmit = (newQuery) => {
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <div className={styles.app}>
+      <SearchBar onSubmit={handleSearchSubmit} />
+      {error && <ErrorMessage message={error} />}
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {loading && <Loader />}
+      {images.length > 0 && !loading && <LoadMoreBtn onClick={handleLoadMore} />}
+      <ImageModal isOpen={!!selectedImage} onRequestClose={closeModal} image={selectedImage} />
+    </div>
+  );
+};
+
+export default App;
+
